e7cc19a3123d7693c5e2ab44a585bf6008c0db62
tomerlich <tomer.erlich2@gmail.com>
2018-09-24 19:27:41 -0700

    Commit

--- src/CIS35ALAB1.java
+++ src/CIS35ALAB1.java
@@ -1,4 +1,13 @@
 
 public class CIS35ALAB1 {
+	public static double monthlyPayment(double LoanAmount, double monthlyRate, double loanTimeYears) {
+		double monthlyPayment = 0.0;
+		
+		return monthlyPayment;
+	}
+	
+	public static void main(String[] Args) {
+		
+	};
 
 }
