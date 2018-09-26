f17d95b12952f386e25fd44af21bf724153f6e81
tomerlich <tomer.erlich2@gmail.com>
2018-09-26 13:02:07 -0700

    commit 8 test from laptop

--- src/CIS35ALAB1.java
+++ src/CIS35ALAB1.java
@@ -63,7 +63,7 @@ public class CIS35ALAB1 {
 			interestAmount = monthlyRate * acountBalance;
 			principleAmount = monthlyPayment - interestAmount;
 			acountBalance = acountBalance - principleAmount;
-			System.out.printf("%8d\t%8s\t%9s\t%s\n",
+			System.out.printf("%8d\t%8s\t%9s\t%7s\n",
 							paymentNum + 1,
 							currencyFormat.format(interestAmount),
 							currencyFormat.format(principleAmount),
@@ -87,6 +87,7 @@ public class CIS35ALAB1 {
 		//this will take two number formats and all of the data.
 		printSchedule(currencyFormat, monthlyPaymentVal);
 		
+		//to prevent memory leaks we have to close the scanner other variables will get automatically garbage collected.
 		sc.close();
 		return;
 	};
